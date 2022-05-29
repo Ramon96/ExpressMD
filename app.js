b5918e0cc439c825d4bbfa5d4501e4e8c7f9a24f
ramon96 <rfm1@live.nl>
2022-05-29 15:52:11 +0200

    dynamic zip name and songs

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+const bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      title: 'Youtube Downloader',
+      version: '1.0.0',
+      description: 'Youtube Downloader API',
+    },
+    host: 'localhost:3000',
+    basePath: '/',
+    produces: ['application/json'],
+    schemes: ['http', 'https'],
+    securityDefinitions: {
+      bearer: {
+        type: 'apiKey',
+        name: 'Authorization',
+        in: 'header',
+      },
+    },
+  },
+  apis: ['./routes/*.js'],
+};
+
 
 var app = express();
 
@@ -13,7 +40,16 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// allow CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -21,6 +57,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJsdoc(swaggerOptions)));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -4,21 +4,27 @@ const fs = require('fs');
 const ytdl = require('ytdl-core');
 var AdmZip = require("adm-zip");
 
-//const sampleData = ['k3bunkZJ4c4', 'X8WeCtU_cuQ', '88g3YrG0HtY', 'H2C9JoTJlHw', 'cphMyCM7JfA', 'f-LUH-AndV0', 'PBWLsUPs2i0', 'NhWORAzjm3k', 'ibyrCG-RRi4', 'Xu1rGfQCu9o', 'GBJDHzLz16Q', 'rQ96NUJrsBg', '-6uBXQjT5Yg', 'zefRH1-kUlg', 'S3y6dpW4-AQ', 'qlIMdDUwZNI', '5kVLFs3Fdv0', 'CMuSlI4NRZY', 'WpyIyuLfdpE', 'gn71phi7YrU', '4r78JWnX37Q', 'jmpIpws5EVE', 'dtlfBW2oRhw', 'jpUhiX-8_qs', 'HF3D9YWBVgU', 'QbgIlsTKF6U', 'FNiaQynpNWU', 'e2nS8MUSVlc', 'CAfrmorosFo', 'F1uxC5GLi_4', 'xILHGOFoViU', 'FXvBqvNICO0', 'SYdr1tWWoqQ', 'BZ8sVLwdX2g', 'bUJ2OPF1Mc4', 'cRmAoolmGHY', '78BJRwiGK-o', 'udAHeLovEME', '9LfVP3ILwxw', 'i6Uc3gVkZNY', 'Hw3__9gY2v0', '7GDm_iDptrc', 'h4Uur_jAStU', 'JlJpFzLpw2I', 'thhmzSgUhU4', 'vd6xM9DEE4c', 'y4sUFF-qjZY', 'wZb2LWTljXE', '7W9IOhk1-z4', 'GUMegkHvF_c'];
-const sampleData = ['k3bunkZJ4c4', 'X8WeCtU_cuQ', '88g3YrG0HtY'];
 const COOKIE = 'YSC=W0QppGN9uEM; CONSENT=YES+yt.427436887.en+FX+070; VISITOR_INFO1_LIVE=-ee-Yb5K8Cs; HSID=AxYV37TMaFeiVBoC0; SSID=Apd3cNHGMHxh3NB-r; APISID=oGkpIs6vurg9no2A/AmVafRsjnFuE_pSL6; SAPISID=P8clkdSNI_bWlQy7/AhZEZWk52CxzRhFkr; __Secure-1PAPISID=P8clkdSNI_bWlQy7/AhZEZWk52CxzRhFkr; __Secure-3PAPISID=P8clkdSNI_bWlQy7/AhZEZWk52CxzRhFkr; LOGIN_INFO=AFmmF2swRAIgUF9kR-wh23QU6RO56vz_r8SeS4cI_5bKRQzebHnR-8QCIEDugpJrrKzI-CJ5eXyKeVa8dDX4xq_Vp81i2EgIt_wH:QUQ3MjNmeUZmV3YzTzdPM1ZRNlF0RDRPRDZHTWJMSFVaNXVfdGU1b1NNY3JRN1dlZE0ybjctOEoyZ2VFMDkwY0NxREtuMjkxSmE5VzVrdEJDb0FWb1JZWHAyR0ROeDBCaUJFU0dWbmdMNW5NT2pqclpvOEt4NDZwRi00dHRFaEZfRl90YjZYeWdIeW5yRTZxOHZuMVV0N19nSFUxU2tneHJiNDJsOF9zWllkR2RQUDhXTG5EUXlreVRweUdHYi13YUNEc1QwRzA5REI2UXVIRGE2WWlKR3dkRFNGN1VTNF9YZw==; SID=KAhdJgPjrAmQXxt9s4w112gdu9Qo7Ptv4QSuFGBorNrpx3Q_2ziqDIJbvpNDpyjbhlxUJg.; __Secure-1PSID=KAhdJgPjrAmQXxt9s4w112gdu9Qo7Ptv4QSuFGBorNrpx3Q_N5kV4DpGLvDUUSK1arUBaw.; __Secure-3PSID=KAhdJgPjrAmQXxt9s4w112gdu9Qo7Ptv4QSuFGBorNrpx3Q_GKuy7Q006B7PSxvfyWP9Kg.; wide=1; PREF=tz=Europe.Amsterdam&f6=40000000&f5=20000&volume=35; SIDCC=AJi4QfEf6PMW6Zel-RMCM6jvxfQ9O4-Ya7BtMhJ5WNe9FyubybxV041CUasJQD6TBa1Xe37xq_4; __Secure-3PSIDCC=AJi4QfGV2ofJTIuTmiP2y_pYN3kwxY34H7PmVGDr1Z70t-ZBgZyDHRI6-ecZvlgZk_LqH6dMjvs';
 
+router.post('/', async (req, res, next) => {
+    const playlistName = req.body.name;
+    const playlistSongs = req.body.songs;
 
-/* GET home page. */
-router.get('/', async (req, res, next) => {
+    if(!playlistName || !playlistSongs) {
+        res.status(400).send('Missing playlist name or songs');
+        return;
+    }
+
     try {
       const getVideos = async () => {
-        const videos = Promise.all(sampleData.map(id => ytdl.getInfo(`https://www.youtube.com/watch?v=${id}`)));
+        const videos = Promise.all(playlistSongs.map(id => ytdl.getInfo(`https://www.youtube.com/watch?v=${id}`)));
         return videos;
       }
 
       getVideos().then(async videos => {
+  
         const downloadVideos = await videos.map( async video => {
+
           let file = `${video.videoDetails.title}`;
           var cleanFileName = file.replace(/[^a-zA-Z0-9]/g, '_');
 
@@ -68,7 +74,7 @@ router.get('/', async (req, res, next) => {
         zip.addLocalFolder('./songs');
 
         // save the zip file
-        zip.writeZip('./songs/songs.zip');
+        zip.writeZip(`./songs/${playlistName}.zip`);
 
         // remove the songs from the folder
         filteredSongs.forEach(song => {
@@ -76,8 +82,8 @@ router.get('/', async (req, res, next) => {
         });
 
         // send the zip file to the client and remove it from the server
-        res.download('./songs/songs.zip', 'songs.zip', () => {
-          fs.unlinkSync('./songs/songs.zip');
+        res.download(`./songs/${playlistName}.zip`, `${playlistName}.zip`, () => {
+          fs.unlinkSync(`./songs/${playlistName}.zip`);
         });
       })
     }
